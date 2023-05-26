f77eccdb7be7c09093679f275993f34ff69e2f4b
SofiaCarv <sofiacr2005@gmail.com>
2023-05-26 19:38:39 -0300

    First commit

---
+++ javascript/header.js
@@ -0,0 +1,134 @@
+/**
+ * @Author: wellingtonmigliari
+ * @Date:   2022-10-11T17:26:33+02:00
+ * @Last modified by:   wellingtonmigliari
+ * @Last modified time: 2023-04-26T21:43:24+02:00
+ */
+
+header.innerHTML = `
+<div class="area">
+</div>
+<nav class="main-menu">
+<li style="width: 0px;">
+	<div class="dropdown-2">
+		<a href="index.html" style="height: 68px;"><i class="fa fa-home fa-2x" style="font-size: 2.9em; margin-left: 12px; margin-right: 12px; margin-top: 20px; color: #f3f4f3;"></i></a>
+	</div>
+</li>
+<li style="width: 0px;">
+	<div class="dropdown-2" style="margin-top: 5px; margin-bottom: 5px;">
+		<button class="dropbtn-2"><i class="fa fa-duotone fa-people-group fa-2x" style="font-size: 2.1em; margin-left: -4px; color: #f3f4f3;"></i></button>
+			<div class="dropdown-content-2">
+				<a href="sobre_crisp.html" style="height: 68px;">sobre</a>
+				<a href="equipe.html" style="height: 68px;">equipe</a>
+			</div>
+	</div>
+</li>
+<li style="width: 0px;">
+	<div class="dropdown-2" style="margin-top: 5px;">
+		<button class="dropbtn-2"><i class="fa fa-calendar-alt fa-2x" style="color: #f3f4f3;"></i></button>
+			<div class="dropdown-content-2">
+				<a href="eventos.html" style="height: 68px;">eventos</a>
+			</div>
+	</div>
+</li>
+<li style="width: 0px;">
+	<div class="dropdown-2">
+		<button class="dropbtn-2"><i class="fa fa-regular fa-podcast fa-2x" style="color: #f3f4f3;"></i></button>
+			<div class="dropdown-content-2">
+			<a href="midia_noticias.html" style="height: 68px;">imprensa</a>
+			<a href="entrevistas.html" style="height: 68px;">podcasts</a>
+			</div>
+	</div>
+</li>
+<li style="width: 0px;">
+	<div class="dropdown-2">
+		<button class="dropbtn-2"><i class="fa fa-solid fa-book fa-2x" style="color: #f3f4f3;"></i></button>
+			<div class="dropdown-content-2">
+				<a href="publicacoes_crisp_artigos.html" style="height: 68px;">artigos</a>
+				<a href="publicacoes_crisp_livros_teses_dissertacoes.html" style="height: 68px;">livros</a>
+				<a href="publicacoes_crisp_livros_teses_dissertacoes.html#fh5co-teses-dissertacoes" style="height: 68px;">teses & dissertações</a>
+			</div>
+	</div>
+</li>
+
+<li style="width: 0px;">
+	<div class="dropdown-2">
+		<button class="dropbtn-2"><i class="fa fa-solid fa-graduation-cap fa-2x" style="color: #f3f4f3;"></i></button>
+			<div class="dropdown-content-2">
+				<a href="pos_graduacao.html" style="height: 68px;">pós-graduação</a>
+				<a href="especializacao.html" style="height: 68px;">especialização</a>
+			</div>
+	</div>
+</li>
+<li style="width: 0px;">
+	<div class="dropdown-2">
+		<button class="dropbtn-2"><i class="fa fa-solid fa-book-open-reader fa-2x" style="color: #f3f4f3;"></i></button>
+			<div class="dropdown-content-2" style="min-width: 224px;">
+				<a href="cade_meus_direitos.html" style="height: 68px;">cadê meus direitos?</a>
+				<a href="erasmus.html" style="height: 68px;">erasmus-success</a>
+				<a href="politicas_publicas.html" style="height: 68px;">políticas públicas CRISP</a>
+				<a href="projetos.html" style="height: 68px;">todos os projetos</a>
+			</div>
+	</div>
+</li>
+
+
+<li>
+	<div class="dropdown-2">
+		<button class="dropbtn-2"><i class="fa fa-solid fa-timeline fa-2x" style="color: #f3f4f3;"></i></button>
+		<div class="dropdown-content-2" style="min-width: 224px;">
+		<a href="linha_do_tempo_unidades_prisionais.html" style="height: 68px;">sobre unidades prisionais</a>
+		<a href="linha_do_tempo.html" style="height: 88px;">sobre legislação para as prisões</a>
+		<a href="linha_do_tempo_fatos_marcantes.html" style="height: 88px;">fatos marcantes para o sistema prisional mineiro</a>
+		</div>
+	</div>
+</li>
+
+
+<li>
+	<div class="dropdown-2">
+		<button class="dropbtn-2"><i class="fa fa-bar-chart-o fa-2x" style="color: #f3f4f3;"></i></button>
+			<div class="dropdown-content-2">
+				<a href="dados.html" style="height: 68px;">referências de dados</a>
+				<!--<a href="#" style="height: 68px;">mapas</a>-->
+			</div>
+	</div>
+</li>
+<li style="width: 0px;">
+	<div class="dropdown-2">
+		<button class="dropbtn-2"><i class="fa fa-solid fa-virus-covid fa-2x" style="color: #f3f4f3;"></i></button>
+			<div class="dropdown-content-2">
+				<a href="covid.html" style="height: 68px;">covid-19</a>
+			</div>
+	</div>
+</li>
+<!--
+<li style="width: 0px;">
+	<div class="dropdown-2">
+		<button class="dropbtn-2"><i class="fa fa-map-marker fa-2x" style="color: #f3f4f3;"></i></button>
+			<div class="dropdown-content-2">
+				<a href="#" style="height: 68px;">mapas</a>
+			</div>
+	</div>
+</li>
+-->
+<li style="width: 0px;">
+	<div class="dropdown-2">
+		<button class="dropbtn-2"><i class="fa fa-info fa-2x" style="color: #f3f4f3;"></i></button>
+			<div class="dropdown-content-2">
+				<a href="index.html#fh5co-started" style="height: 68px;">newsletter</a>
+			</div>
+	</div>
+</li>
+<!--
+<li style="width: 0px;">
+	<div class="dropdown-2">
+		<button class="dropbtn-2"><i class="fa fa-search fa-2x" style="color: #f3f4f3;"></i></button>
+			<div class="dropdown-content-2">
+				<a href="search.html" style="height: 68px;">pesquisar</a>
+			</div>
+	</div>
+</li>
+-->
+</nav>
+`;
